9dd7301bc5aa4a4b96d56095760d775a3d088021
Kenny Lepping <kennylepping@gmail.com>
2020-12-04 04:25:22 -0500

    Creating an e-commerce website.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,68 @@
+from django.conf import settings
 from django.db import models
+from django.shortcuts import reverse
 
-# Create your models here.
+CATEGORY_CHOICES = (
+    ('S','Shirt'), # First parameter is what goes in the DB and 2nd is what's displayed
+    ('SW','Sportwear'),
+    ('OW','Outwear')
+)
+
+LABEL_CHOICES = (
+    ('P','primary'), # First parameter is what goes in the DB and 2nd is what's displayed
+    ('S','secondary'), # primary, secondary... are from the Bootstrap CSS colors
+    ('D','danger')
+)
+
+# An Item becomes an OrderItem when in the shopping cart 
+class Item(models.Model): # Way of linking between the order and the item itself
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+
+    # blank and null are true so we don't have to have a discount price
+    discount_price = models.FloatField(blank=True, null=True)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    label = models.CharField(choices=LABEL_CHOICES, max_length=1)
+    slug = models.SlugField()
+    description = models.TextField()    
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("core:product", kwargs={
+            'slug': self.slug
+        })
+
+    def get_add_to_cart_url(self):
+        return reverse("core:add-to-cart", kwargs={
+            'slug': self.slug
+        })
+    
+    def get_remove_from_cart_url(self):
+        return reverse("core:remove-from-cart", kwargs={
+            'slug': self.slug
+        }) 
+
+
+class OrderItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, 
+                             on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.item.title}"
+
+
+class Order(models.Model): # Is like the shopping cart, all order items go in here
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
--- core/views.py
+++ core/views.py
@@ -1,3 +1,103 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import F
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, View
+from django.utils import timezone
 
-# Create your views here.
+from .models import Item, OrderItem, Order
+
+
+def products(request):
+    context = {
+        'items':Item.objects.all()     
+    }
+    return render(request, "product.html", context)
+
+
+def checkout(request):
+    return render(request, "checkout.html")
+
+
+class HomeView(ListView):
+    model = Item
+    paginate_by = 10 # When is 1 - shows page 1 and 2
+    template_name = "home.html"
+
+
+class OrderSummaryView(LoginRequiredMixin, View):
+    def get(self, *args, **kwargs):
+        try:
+            order = Order.objects.get(user=self.request.user, ordered=False)
+            context = {
+                'object': order
+            }
+            return render(self.request, 'order_summary.html', context)
+        except ObjectDoesNotExist:
+            messages.error(self.request, "You do not have an active order.")
+            return redirect("/")
+
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = "product.html"
+
+ 
+@login_required
+def add_to_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_item, created = OrderItem.objects.get_or_create(
+        item=item,
+        user=request.user,
+        ordered=False
+    )
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.items.filter(item__slug=item.slug).exists():
+        #    order_item.quantity += 1 # Was order_item.quantity += 1
+            order_item.quantity = F('quantity') + 1 # From YT comment to avoid race condition
+            order_item.save()
+            messages.info(request, "This item quantity was updated.")
+            return redirect("core:product", slug=slug)
+        else:
+            order.items.add(order_item)
+            messages.info(request, "This item was added to your cart.")
+            return redirect("core:product", slug=slug)
+    else:
+        ordered_date = timezone.now()
+        order = Order.objects.create(
+            user=request.user, ordered_date=ordered_date)
+        order.items.add(order_item)
+        messages.info(request, "This item was added to your cart.")
+        return redirect("core:product", slug=slug)
+
+
+@login_required
+def remove_from_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_qs = Order.objects.filter(
+        user=request.user,
+        ordered=False,
+    )
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.items.filter(item__slug=item.slug).exists():        
+            order_item = OrderItem.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0]
+            order.items.remove(order_item)         
+            messages.info(request, "This item was removed from your cart.")
+            return redirect("core:product", slug=slug)
+        else:
+            messages.info(request, "This item was not in your cart.")
+            return redirect("core:product", slug=slug)
+    else:
+        messages.info(request, "You do not have an active order.")
+        return redirect("core:product", slug=slug)
