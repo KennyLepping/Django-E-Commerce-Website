8bb0eacdf1891c55d49a7bc20c1620a544a29367
Kenny Lepping <kennylepping@gmail.com>
2020-12-04 23:27:57 -0500

    Users can now add and remove items from within their cart updating their quantities.

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,8 @@ from .views import (
     HomeView,
     OrderSummaryView,
     add_to_cart,
-    remove_from_cart
+    remove_from_cart,
+    remove_single_item_from_cart
 )
 
 app_name = 'core'
@@ -16,6 +17,7 @@ urlpatterns = [
     path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
     path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
-    path('remove_from_cart/<slug>/', remove_from_cart, name='remove-from-cart')
-
+    path('remove_from_cart/<slug>/', remove_from_cart, name='remove-from-cart'),
+    path('remove_item_from_cart/<slug>/', remove_single_item_from_cart, 
+         name='remove-single-item-from-cart')
 ]
--- core/views.py
+++ core/views.py
@@ -57,23 +57,21 @@ def add_to_cart(request, slug):
     if order_qs.exists():
         order = order_qs[0]
         # check if the order item is in the order
-        if order.items.filter(item__slug=item.slug).exists():
-        #    order_item.quantity += 1 # Was order_item.quantity += 1
-            order_item.quantity = F('quantity') + 1 # From YT comment to avoid race condition
-            order_item.save()
+        if order.items.filter(item__slug=item.slug).exists():        
+            order_item.quantity = F('quantity') + 1 # Was += 1, refer to https://docs.djangoproject.com/en/3.1/ref/models/expressions/
             messages.info(request, "This item quantity was updated.")
-            return redirect("core:product", slug=slug)
+            return redirect("core:order-summary")
         else:
             order.items.add(order_item)
             messages.info(request, "This item was added to your cart.")
-            return redirect("core:product", slug=slug)
+            return redirect("core:order-summary")
     else:
         ordered_date = timezone.now()
         order = Order.objects.create(
             user=request.user, ordered_date=ordered_date)
         order.items.add(order_item)
         messages.info(request, "This item was added to your cart.")
-        return redirect("core:product", slug=slug)
+        return redirect("core:order-summary")
 
 
 @login_required
@@ -94,7 +92,38 @@ def remove_from_cart(request, slug):
             )[0]
             order.items.remove(order_item)         
             messages.info(request, "This item was removed from your cart.")
+            return redirect("core:order-summary")
+        else:
+            messages.info(request, "This item was not in your cart.")
             return redirect("core:product", slug=slug)
+    else:
+        messages.info(request, "You do not have an active order.")
+        return redirect("core:product", slug=slug)
+
+
+@login_required
+def remove_single_item_from_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_qs = Order.objects.filter(
+        user=request.user,
+        ordered=False,
+    )
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.items.filter(item__slug=item.slug).exists():        
+            order_item = OrderItem.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0]
+            if order_item.quantity > 1:
+                order_item.quantity = F('quantity') - 1 # Was -= 1, refer https://docs.djangoproject.com/en/3.1/ref/models/expressions/
+                order_item.save()
+            else:
+                order.items.remove(order_item)                
+            messages.info(request, "This item quantity was updated.")
+            return redirect("core:order-summary")
         else:
             messages.info(request, "This item was not in your cart.")
             return redirect("core:product", slug=slug)
