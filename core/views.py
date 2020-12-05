1e160c0a11d827897f4a2fb4da9fc16b61355445
Kenny Lepping <kennylepping@gmail.com>
2020-12-05 05:16:33 -0500

    Working on the checkout form

--- core/views.py
+++ core/views.py
@@ -5,9 +5,10 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import F
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, View
+from .forms import CheckoutForm
 from django.utils import timezone
 
-from .models import Item, OrderItem, Order
+from .models import Item, OrderItem, Order, BillingAddress
 
 
 def products(request):
@@ -17,8 +18,36 @@ def products(request):
     return render(request, "product.html", context)
 
 
-def checkout(request):
-    return render(request, "checkout.html")
+class CheckoutView(View):
+    def get(self, *args, **kwargs):
+        # form
+        form = CheckoutForm()
+        context = {
+            'form': form    
+        }
+        return render(self.request, "checkout.html", context)
+
+    def post(self, *args, **kwargs):
+        form = CheckoutForm(self.request.POST or None)        
+        if form.is_valid():
+            street_address = form.cleaned_data.get('street_address')
+            apartment_address = form.cleaned_data.get('apartment_address')
+            country = form.cleaned_data.get('country')
+            zip = form.cleaned_data.get('zip')
+            same_billing_address = form.cleaned_data.get(
+                'same_billing_address')
+            save_info = form.cleaned_data.get('save_info')
+            payment_option = form.cleaned_data.get('payment_option')
+            #billing_address = BillingAddress(
+            #    user=self.request.user, 
+            #    street_address=street_address,
+            #    apartment_address=apartment_address,
+            #    country=country,
+            #    zip=zip
+            #)
+            return redirect('core:checkout')
+        messages.warning(self.request, "Failed to checkout")
+        return redirect('core:checkout')
 
 
 class HomeView(ListView):
